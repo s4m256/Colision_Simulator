35005dbefd1894f0ce0485b403a77f6d7b34d44d
samuel-santiago <al.2312.samuelsantiago@gmail.com>
2025-12-23 23:30:45 -0300

    Initial commit

---
+++ particle.js
@@ -0,0 +1,67 @@
+class Particle {
+    constructor (x, y, mass, velocity) {
+        this.position = createVector(x, y)
+        this.velocity = velocity
+        this.mass = mass
+        this.radius = Math.sqrt(this.mass / PI) * SCALE
+        this.color = color(random(0,255), random(0,255), random(0,255))
+    }
+
+    draw() {
+        noStroke()
+        fill(this.color)
+        ellipse(this.position.x, this.position.y, this.radius * 2)
+    }
+
+    physics(particle) {
+        let distanceVec = p5.Vector.sub(particle.position, this.position);
+        let distance = distanceVec.mag();
+        let radius = this.radius + particle.radius;
+
+        if (distance >= radius) return;
+
+        let normal;
+        if (distance === 0) {
+            normal = createVector(random(-1,1), random(-1,1)).normalize();
+        } else {
+            normal = distanceVec.copy().normalize();
+        }
+
+        let relativeVelocity = p5.Vector.sub(this.velocity, particle.velocity);
+        let speed = relativeVelocity.dot(normal);
+
+        if (speed <= 0) return;
+
+        let impulse = (2 * speed) / (1 / this.mass + 1 / particle.mass); 
+        let impulseVec = normal.copy().mult(impulse);
+
+        this.velocity.sub(p5.Vector.mult(impulseVec, 1 / this.mass));
+        particle.velocity.add(p5.Vector.mult(impulseVec, 1 / particle.mass));
+
+        let overlap = 0.5 * (radius - distance);
+        this.position.sub(p5.Vector.mult(normal, overlap));
+        particle.position.add(p5.Vector.mult(normal, overlap));
+    }
+
+
+    update() {
+        this.position.add(p5.Vector.mult(this.velocity, deltaTime));
+
+        if (this.position.x - this.radius < 0) {
+            this.position.x = this.radius; 
+            this.velocity.x *= -1;         
+        }
+        if (this.position.x + this.radius > width) {
+            this.position.x = width - this.radius;
+            this.velocity.x *= -1;
+        }
+        if (this.position.y - this.radius < 0) {
+            this.position.y = this.radius;
+            this.velocity.y *= -1;
+        }
+        if (this.position.y + this.radius > height) {
+            this.position.y = height - this.radius;
+            this.velocity.y *= -1;
+        }
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,32 @@
+const e = 1
+const SCALE = 0.003
+
+let particles = []
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    for (let i = 0; i < 10; i++) {
+        let x = random(0, width);
+        let y = random(0, height);
+        let mass = random(2e8, 1e9);
+        let velocity = createVector(random(-1,1), random(-1,1));
+
+        particles.push(new Particle(x, y, mass, velocity));
+    }
+}
+
+function draw() {
+    background(51, 51, 51)
+
+    for(let i=0; i<particles.length; i++){
+        for(let j=i+1;j<particles.length;j++){
+            particles[i].physics(particles[j]);
+        }
+    }
+    
+    for(const particle of particles) {
+        particle.update()
+        particle.draw()
+    }
+}
